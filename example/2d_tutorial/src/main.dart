4e45c701347a385a8397c6c1afbb85e9e071672f
Jeff Ward <jeff@fuzzybinary.com>
2024-02-18 23:18:46 -0500

    🗑 Remove old custom node

--- example/2d_tutorial/src/main.dart
+++ example/2d_tutorial/src/main.dart
@@ -1,10 +1,7 @@
 import 'package:godot_dart/godot_dart.dart';
 
 import 'godot_dart_scripts.g.dart';
-import 'lib/simple.dart';
 
 void main() {
-  Simple.bind();
-
   attachScriptResolver(DartScriptLanguage.singleton);
 }
